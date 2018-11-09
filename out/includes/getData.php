c5f33585c80ef08cc15c44e942d0c53ee4580c44
andrei sandru <andy310584@gmail.com>
2018-11-09 18:45:48 +0200

    getData

--- out/includes/getData.php
+++ out/includes/getData.php
@@ -1,8 +1,8 @@
 <?php
-   $host        = "host = localhost";
-   $port        = "port = 5000";
-   $dbname      = "dbname = postgres";
-   $credentials = "user = postgres password=sb1947ll";
+   $host        = "host = ec2-54-225-98-131.compute-1.amazonaws.com";
+   $port        = "port = 5432";
+   $dbname      = "dbname = d1uk2vnpdjl28q";
+   $credentials = "user = npvswhggxgsgxk password=556e5013ea1dec59d2daa5d7bff223ec028c16da4ac577db94e78dbc75471965";
    $sslmode = "sslmode = require";
 
    $db = pg_connect( "$host $port $dbname $credentials $sslmode"  );
