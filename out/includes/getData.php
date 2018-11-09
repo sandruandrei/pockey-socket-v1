d1a8a113b39dc3a77fa182e9a012c108666ba8bd
andrei sandru <andy310584@gmail.com>
2018-11-09 17:43:19 +0200

    getData.php update. test6 heroku

--- out/includes/getData.php
+++ out/includes/getData.php
@@ -9,29 +9,29 @@ else
 {
     $idToCheck = filter_input(INPUT_POST, '$idToCheck');
 
-    $con = mysqli_connect("localhost", "sandruan_sandru", "d,gd.!z=2zD7", "sandruan_proteste");
+    $con = pg_connect("host=ec2-54-225-98-131.compute-1.amazonaws.com port=5432 dbname=d1uk2vnpdjl28q user=npvswhggxgsgxk password=556e5013ea1dec59d2daa5d7bff223ec028c16da4ac577db94e78dbc75471965");
 
     //checking
-    $idVerification = mysqli_query($con, "SELECT USER_ID FROM pockey_table WHERE USER_ID='" . $idToCheck . "'");
-
-    if (!$idVerification) {
-        echo 'salam2';
-        die('Error: ' . mysqli_error($con));
-    }
-
-    if (mysqli_num_rows($idVerification) > 0) {
-
-        //email already exists
-        $ratedQuery = mysqli_query($con, "SELECT rated FROM proteste_table WHERE USER_ID = '" . $idToCheck . "'") or die('nu vrea');
-
-        while ($ratedResult = mysqli_fetch_assoc($ratedQuery)) {
-            echo $ratedResult["rated"];
-        }
-
-    } else {
-        //email doesn't exist
-        echo 'salam3';
-    }
+//    $idVerification = mysqli_query($con, "SELECT USER_ID FROM pockey_table WHERE USER_ID='" . $idToCheck . "'");
+//
+//    if (!$idVerification) {
+//        echo 'salam2';
+//        die('Error: ' . mysqli_error($con));
+//    }
+//
+//    if (mysqli_num_rows($idVerification) > 0) {
+//
+//        //email already exists
+//        $ratedQuery = mysqli_query($con, "SELECT rated FROM proteste_table WHERE USER_ID = '" . $idToCheck . "'") or die('nu vrea');
+//
+//        while ($ratedResult = mysqli_fetch_assoc($ratedQuery)) {
+//            echo $ratedResult["rated"];
+//        }
+//
+//    } else {
+//        //email doesn't exist
+//        echo 'salam3';
+//    }
 }
 
 
