75b4f217ea936f95fa1a2ec1027f7d5e793d2cf0
andrei sandru <andy310584@gmail.com>
2018-11-09 19:37:57 +0200

    getData. heroku multi build pack created: node + php

--- out/includes/getData.php
+++ out/includes/getData.php
@@ -1,3 +1,16 @@
 <?php
    echo "salam";
+
+   $host        = "host = ec2-54-225-98-131.compute-1.amazonaws.com";
+      $port        = "port = 5432";
+      $dbname      = "dbname = d1uk2vnpdjl28q";
+      $credentials = "user = npvswhggxgsgxk password=556e5013ea1dec59d2daa5d7bff223ec028c16da4ac577db94e78dbc75471965";
+      $sslmode = "sslmode = require";
+
+      $db = pg_connect( "$host $port $dbname $credentials $sslmode"  );
+      if(!$db) {
+         echo "Error : Unable to open database\n";
+      } else {
+         echo "Opened database successfully\n";
+      }
 ?>
