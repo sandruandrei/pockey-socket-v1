1ad67b0bcf62824d44b9e2a2285029f0456d8bbf
andrei sandru <andy310584@gmail.com>
2018-11-09 18:40:50 +0200

    getData

--- out/includes/getData.php
+++ out/includes/getData.php
@@ -1,8 +1,14 @@
 <?php
-$connection = pg_connect ("host=ec2-54-225-98-131.compute-1.amazonaws.com port=5432 dbname=d1uk2vnpdjl28q user=npvswhggxgsgxk password=556e5013ea1dec59d2daa5d7bff223ec028c16da4ac577db94e78dbc75471965");
-if($connection) {
-    echo 'connected';
-} else {
-    echo 'there has been an error connecting';
-}
+   $host        = "host = localhost";
+   $port        = "port = 5000";
+   $dbname      = "dbname = postgres";
+   $credentials = "user = postgres password=sb1947ll";
+   $sslmode = "sslmode = require";
+
+   $db = pg_connect( "$host $port $dbname $credentials $sslmode"  );
+   if(!$db) {
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
 ?>
