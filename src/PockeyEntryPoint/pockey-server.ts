2bc02083eb8224bd046d8d9afa7faad05a94add5
andrei sandru <andy310584@gmail.com>
2018-11-16 16:21:05 +0200

    v2 - update

--- src/PockeyEntryPoint/pockey-server.ts
+++ src/PockeyEntryPoint/pockey-server.ts
@@ -6,6 +6,9 @@ import http            = require( "http" );
 import socketIO        = require( 'socket.io' );
 //@ts-ignore
 import express        = require( 'express' );
+//@ts-ignore
+import pg        = require( 'pg' );
+
 
 export module PockeyServer {
 
@@ -21,49 +24,92 @@ export module PockeyServer {
         protected httpServer;
         protected socketIo;
         protected socketIsFree: boolean = true;
+        protected databaseConnected: boolean = false;
+        protected databasePool: pg.Pool;
 
         // protected lookingForPartner: boolean = true;
 
         constructor() {
-            // let express = require('express');
-            const app = express();
+            this.databasePool = new pg.Pool({
+                user: 'npvswhggxgsgxk',
+                host: 'ec2-54-225-98-131.compute-1.amazonaws.com',
+                database: 'd1uk2vnpdjl28q',
+                password: '556e5013ea1dec59d2daa5d7bff223ec028c16da4ac577db94e78dbc75471965',
+                port: 5432,
+                ssl: true
+            });
 
-            /* // var app = new express();
-             // var http = http.Server(app);
-             // var app = require('express')();
-             const express = require('express')
-             const app = express()
-             const port = 4000
 
-             app.get('/', (req, res) => res.send('Hello World!'));
-             app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-             // var http = new (require('http')).Server(app);
+            this.databasePool.on('error', (err, client) => {
+                console.error('Unexpected error on idle client', err);
+                process.exit(-1);
+            });
 
-             // app.get('/', function(req, res){
-             //     res.send('<h1>Hello world</h1>');
-             // });
+            console.log("bla");
 
-             // http.listen(4000, function(){
-             //     console.log('listening on *:3000');
-             // });*/
-//-------------------
-            this.file = new nodeStatic.Server('../out', { // bin is the folder containing our html, etc
+            this.databasePool.connect((err, client, done) => {
+                if (err) {
+                    throw err;
+                }
+                else {
+                    console.log("s-a logat, merge.");
+                    this.databaseConnected = true;
+                }
+            });
+
+            ////////////////////////////////
+            // const app = express();
+
+            this.file = new nodeStatic.Server('out', { // bin is the folder containing our html, etc
                 cache: 0,	// don't cache
                 gzip: true	// gzip our assets
             });
 
+            let port = process.env.PORT || 4000;
             // create our server
             this.httpServer = http.createServer((request, response) => {
                 request.addListener('end', () => {
                     this.file.serve(request, response);
                 });
                 request.resume();
-            }).listen(4000);
+            });
+
+            // var http=require('http');
+
+            // var server=http.createServer(function(req,res){
+            //     res.end('test');
+            // });
+
+            this.httpServer.on('listening',function(){
+                console.log('ok, server is running');
+            });
+
+            this.httpServer.listen(port);
 
             this.socketIo = socketIO();
             // console.log("socket id: " + this.socketIo.id);
             this.socketIo.serveClient(true); // the server will serve the client js file
             this.socketIo.attach(this.httpServer);
+
+            this.socketIo.on('connection', (socket) => {
+                console.log('a user connected');
+                //todo aici trebuie sa te ocupi de disconnect dar si pe celalalte socketuri
+                socket.on('disconnect', function () {
+                    console.log('user disconnected');
+                });
+                socket.on(FrameworkSocketEvents.getUserFromDatabase, (username) => {
+                    console.log('someone wants some db in');
+                    if (this.databaseConnected) {
+                        this.checkForUserID(username);
+                    }
+                });
+                socket.on(FrameworkSocketEvents.updateUserData, (data) => {
+                    if (this.databaseConnected) {
+                        this.updateUserDb(data);
+                    }
+                });
+
+            });
 
             let lookingForPartnerNamespace = this.socketIo.of(FrameworkSocketNamespaces.SEARCH);
             lookingForPartnerNamespace.on('connection', function (socket) {
@@ -166,6 +212,90 @@ export module PockeyServer {
              });*/
 
 
+        }
+
+        private updateUserDb(data): void {
+            let sqlText: string = "UPDATE pockey_table SET " + data["column"] + "='" + data["value"] + "' WHERE user_id='" + data["userID"] + "'";
+            console.log("sqlText: " + sqlText);
+
+            (async () => {
+                const client = await this.databasePool.connect()
+                try {
+                    const res = await client.query(sqlText);
+                    this.socketIo.emit(FrameworkSocketEvents.updateUserData);
+                    // console.log(res.rows[0]);
+
+
+                } finally {
+                    client.release();
+                    console.log("client");
+                }
+            })().catch(e => console.log(e.stack))
+
+            /*const query = {
+                // give the query a unique name
+                name: 'update-user',
+                text: sqlText
+            };
+
+            this.databasePool.query(query)
+                /!*.then(res => {
+                    console.log("user updated: " + res.rows[0]);
+                })
+                .catch(e => {
+                    console.error(e.stack)
+                })*!/*/
+        }
+
+
+        private checkForUserID(username): void {
+            console.log("checking db for user_id: " + username);
+            const query = {
+                // give the query a unique name
+                name: 'fetch-user',
+                text: 'SELECT * FROM pockey_table WHERE USER_ID = $1',
+                values: [username]
+            };
+
+            this.databasePool.query(query)
+                .then(res => {
+                    console.log("user retrieved: " + res.rows[0]);
+
+                    if (res.rows[0] == undefined || res.rows[0] == "undefined" || res.rows[0] == null) {
+                        this.createNewUser(username);
+                    }
+                    else {
+                        this.socketIo.emit(FrameworkSocketEvents.getUserFromDatabase, res.rows[0]);
+                    }
+                    // console.log(res.rows[0]); // ['Brian', 'Carlson']
+
+
+                })
+                .catch(e => {
+                    // console.log("intra la error");
+                    /* mysqli_query($con, "INSERT INTO proteste_table (email, rated, poza".$image.") VALUES ('" . $email . "', '" . $image . "', '" . $rating . "')");*/
+                    console.error(e.stack)
+                })
+        }
+
+        private createNewUser(username): void {
+            console.log("creating new user: " + username);
+            const createNewUserQuery =
+                {
+                    name: 'create-new-user',
+                    text: 'INSERT INTO pockey_table(user_id) VALUES($1)',
+                    values: [username]
+                };
+
+            this.databasePool.query(createNewUserQuery)
+                .then(res => {
+                    /* console.log("new user created. user saved into db");
+                     this.socketIo.emit(FrameworkSocketEvents.getUserFromDatabase, res.rows[0]);*/
+                    this.checkForUserID(username);
+                })
+                .catch(e => {
+                    console.error(e.stack)
+                });
         }
     }
 }
